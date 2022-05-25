22c40cf9d63edbbaa6f97a0cde7a2bb0c60e65a6
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-05-25 19:29:57 +0530

    l

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,20 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener("resize", showButton);
 
     return (
         <>
