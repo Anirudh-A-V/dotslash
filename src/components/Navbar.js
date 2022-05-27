2b48d7582af842821acc26fdc61cf9bfe13b8944
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-05-28 01:38:53 +0530

    Fixed the navbar icon.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,8 +45,8 @@ function Navbar() {
                             <Link to='/gaming' className='nav-links' onClick={closeMobileMenu}>Gaming</Link>
                         </li>
                         <li className='nav-item'>
-                            <Link to='/contact-us' className='nav-links-mobile' onClick={closeMobileMenu}></Link>
-                            {button && <Button buttonStyle='btn--outline'>Contact Us</Button>}
+                            {/* <Link to='/contact-us' className='nav-links-mobile' onClick={closeMobileMenu}></Link> */}
+                            {button && <Button buttonStyle='btn--outline'className='nav-links-mobile' onClick={closeMobileMenu} >Contact Us</Button>}
                         </li>
                     </ul>
                 </div>
