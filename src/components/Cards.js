defbdc18a5249f6f7591574624c777f757f21853
Anirudh A V <76550448+Anirudh-A-V@users.noreply.github.com>
2022-06-03 14:43:58 +0530

    d

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import "./Cards.css";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Card 1</h1>
+      <h1>Events</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
